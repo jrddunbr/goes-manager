575b1d8b3829f69f0cd464b57ea3247edc64dcca
Jared Dunbar <jrddunbr@ja4.org>
2025-11-01 02:43:56 -0400

    Don't need the old nginx config

--- scripts/deploy_fancy.sh
+++ scripts/deploy_fancy.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 sudo mkdir -p /var/satellite/satellite_raw/_fancy
 sudo cp -r web/_fancy/. /var/satellite/satellite_raw/_fancy/
 
-sudo cp web/default_with_headers /etc/nginx/sites-available/default
+sudo cp web/goes.conf /etc/nginx/sites-available/default
 sudo nginx -t
 sudo systemctl reload nginx
